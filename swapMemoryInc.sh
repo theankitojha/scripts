ae2e18f8b302ef06fcb3daccef54067e28d4dcca
theankitojha <theankitojha@gmail.com>
2022-05-27 00:18:36 +0545

    Create swapMemoryInc.sh

---
+++ swapMemoryInc.sh
@@ -0,0 +1,11 @@
+ls -lh /swapfile
+sudo chmod 600 /swapfile
+sudo mkswap /swapfile
+ls -lh /swapfile
+ sudo swapon /swapfile
+sudo swapon --show
+free -h
+sudo cp /etc/fstab /etc/fstab.bak
+echo '/swapfile none swap sw 0 0' | sudo tee -a /etc/fstab
+echo 'vm.swappiness=10' | sudo tee -a /etc/sysctl.conf
+echo 'vm.vfs_cache_pressure=50' | sudo tee -a /etc/sysctl.conf
